a857fc8e3e8e8a05332dee8598538a0b32994e6d
Akshay Kumar Patil <akshaykumarpatil58@gmail.com>
2026-05-07 13:46:35 +0530

    message

--- backend/models/message.py
+++ backend/models/message.py
@@ -3,15 +3,10 @@ from typing import Optional,Dict,Any
 
 
 MESSAGE_TYPES = [
-    "join",
     "start",
+    "word_selected",
     "draw",
     "chat",
-    "correct_guess",
-    "word",
-    "word_hint",
-    "score_update",
-    "end_round"
 ]
 
 
@@ -40,50 +35,20 @@ def validate_message(message:Message) -> Optional[Message]:
             
 
         
-    if message.type == "join":
-        if message.username is not None:
-            if message.room_id is not None:
-                return message
-
-    
     if message.type == "start":
-        if message.username is not None:
-            if message.room_id is not None:
-                if isinstance(message.data,dict):
-                    if 'total_time' in message.data and 'word' in message.data:
-                        return message
-                    
-
-
-    if message.type == "correct_guess":
-        if message.username is not None:
-            if isinstance(message.data,dict):
-                if 'time_taken' in message.data and 'points' in message.data:
-                    return message
-        
-
-    if message.type == "word":
-        if message.username is not None:
-            if isinstance(message.data,dict):
-                if 'word' in message.data :
-                    return message
-        
-
-    if message.type == "word_hint":
         if isinstance(message.data,dict):
-                if 'hint' in message.data :
-                    return message
-        
-        
-    if message.type == "score_update":
-        if isinstance(message.data, dict):
-            if "scores" in message.data:
+            if 'total_time' in message.data and 'total_rounds' in message.data:
                 return message
 
-    if message.type == "end_round":
-        if isinstance(message.data, dict):
-            if "correct_word" in message.data and "scores" in message.data:
+    if message.type == "word_selected":
+        if isinstance(message.data,dict):
+            if 'select_word' in message.data:
                 return message
+
         
-            
+    if message.type == "draw":
+        if isinstance(message.data,dict):
+            if 'x' in message.data and 'y' in message.data and 'color' in message.data and 'size' in message.data:
+                return message
+
     return None
