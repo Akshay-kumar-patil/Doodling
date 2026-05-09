96657afefeee3a1783ce8211de20a9936b430c44
Akshay Kumar Patil <akshaykumarpatil58@gmail.com>
2026-05-09 12:58:14 +0530

    models updated

--- backend/models/message.py
+++ backend/models/message.py
@@ -21,20 +21,12 @@ class Message(BaseModel):
 def validate_message(message:Message) -> Optional[Message]:
     if message.type not in MESSAGE_TYPES:
         return None
-    
-    if message.type == "draw":
-        if isinstance(message.data,dict):
-            if 'x' in message.data and 'y' in message.data:
-                return message
-            
-            
+
     if message.type == "chat":
         if isinstance(message.data,dict):
             if "text" in message.data:
                 return message
-            
 
-        
     if message.type == "start":
         if isinstance(message.data,dict):
             if 'total_time' in message.data and 'total_rounds' in message.data:
